608dc982a324a9fe888793d1a65ee24a252a2817
git <82054121+KXzeno@users.noreply.github.com>
2024-04-12 15:06:10 -0700

    REFACTOR: parse events path for SoC

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
-import keys from 'dotenv/config';
+import 'dotenv/config';
 
 //const dir = dirname(import.meta.filename);
 const __filename = fileURLToPath(import.meta.url)
@@ -13,62 +13,55 @@ let client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const commandFoldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(commandFoldersPath);
 
 let importPromises = [];
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
+for await (const folder of commandFolders) {
+	const commandsPath = path.join(commandFoldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const fileURL = new URL(`file://${filePath}`);
 		importPromises.push(
-		import(fileURL).then(module => {
-			const { command } = module;
-			// Set a new item in the Collection with the key as the command name and the value as the exported module
-			if ('data' in command && 'execute' in command) {
-				client.commands.set(command.data.name, command);
-			} else {
-				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-			}
-		}).catch(error => {
-			console.error(`[ERROR] Failed to import command file at ${filePath}: ${error}`);
-		}));
+			import(fileURL).then(module => {
+				const { command } = module;
+				// Set a new item in the Collection with the key as the command name and the value as the exported module
+				if ('data' in command && 'execute' in command) {
+					client.commands.set(command.data.name, command);
+				} else {
+					// console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+				}
+			}).catch(error => {
+				console.error(`[ERROR] Failed to import command file at ${filePath}: ${error}`);
+			}));
 	}
 }
 
-await Promise.all(importPromises);
+await Promise.all(importPromises).then(() => { importPromises = []; });
 
-// Runs once when client is ready
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Logged in as ${readyClient.user.tag}`);
-});
+const eventFilesPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventFilesPath).filter(file => file.endsWith('.js'));
+
+for await (const file of eventFiles) {
+	const filePath = path.join(eventFilesPath, file);
+	const fileURL = new URL(`file://${filePath}`);
+	importPromises.push(
+		import(fileURL).then(module => {
+			const { event } = module;
+			if (event.once) {
+				client.once(event.name, (...args) => event.execute(...args));
+			} else {
+				client.on(event.name, (...args) => event.execute(...args));
+			}
+		}).catch(err => {
+			console.error(`[ERROR] Failed to import command file at ${filePath}: ${err}`);
+		}));
+}
+
+await Promise.all(importPromises).then(() => { importPromises = []; });
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
-
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
