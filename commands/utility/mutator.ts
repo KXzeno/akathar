d71ce06177427c71af14ba97288bd9e17db5d27d
git <82054121+KXzeno@users.noreply.github.com>
2024-11-28 08:36:45 -0800

    REFACTOR!: contain embed creation logic for reuse

--- commands/utility/mutator.ts
+++ commands/utility/mutator.ts
@@ -15,7 +15,7 @@ export const command = {
 
     interface MutationData {
       id: string | number;
-      data: string;
+      data: Response | string;
       name: string;
       map: string;
       mutators: string[];
@@ -25,7 +25,7 @@ export const command = {
     }
 
     let mutationData: MutationData | undefined = undefined;
-    let mutatorField = [];
+    let mutatorField: Array<object | undefined> = [];
 
     function getYDM(dateObj: Date): string {
       // console.log('Capturing date string');
@@ -85,24 +85,24 @@ export const command = {
       return false;
     }
 
-    if (data !== undefined) {
+    async function generateMutationData(): Promise<MutationData | undefined> {
       try {
-        data = await data.text().then(res => res.split('\n')) as string[];
-        mutatorList = await mutatorList.text().then(res => res.split('\n')).then(res => {
+        data = await (data as Response).text().then(res => res.split('\n')) as string[];
+        mutatorList = await (mutatorList as Response).text().then(res => res.split('\n')).then(res => {
           let itemized = [];
           for (let i = 0; i < res.length; i++) {
             if (res[i] !== undefined) itemized.push(res[i].split(','));
           }
           return itemized;
         }) as string[][];
-
+        //
         // Remove labels
         data.shift() && mutatorList.shift() && mutatorList.pop();
 
         let mutation = searchMutator(data) as string[];
-        // console.log(`Mutation: ${mutation}`);
+
         let mutators: Array<string> = [mutation[7], mutation[8], mutation[9]].filter(title => title.length >= 1);
-        mutationData = {
+        return mutationData = {
           id: mutation[0],
           data: mutation[1],
           name: mutation[2],
@@ -112,59 +112,68 @@ export const command = {
           difficulty: mutation[13],
           rating: mutation[14],
         }
-
-        for (let i = 0; i < mutationData.mutators.length; i++) {
-          // console.log(mutatorList.split(',').find(item => item[0] === mutationData.mutators[i + 1])[2])
-          let target = mutationData.mutators[i];
-          if (!mutatorList || !target) return;
-          let desc: string = mutatorList.find(mutator => target === mutator[0])![2];
-          // console.log(desc);
-          mutatorField.push({ name: `Mutator ${i + 1}`, value: `${mutationData.mutators[i]}\n\n${desc}`, inline: true });
-        }
-        // console.log('Mutator Field: ', ...mutatorField);
-        // console.log(mutationData);
-      } catch (e) {
-        // console.log(`ERR: ${e}`);
+      } catch (err) {
+        console.log(`${err}\nUnable to generate mutation data.`);
       }
     }
 
-    let color;
-    if (!mutationData) throw new Error ('No Mutation Data Detected.');
+    async function generateMutatorEmbed(): Promise<EmbedBuilder | undefined> {
+      if (data !== undefined) {
+        try {
+          mutationData = await generateMutationData();
+          if (!data || mutationData === undefined) throw new Error('Unable to generate mutation data.')
+            for (let i = 0; i < mutationData.mutators.length; i++) {
+              // console.log(mutatorList.split(',').find(item => item[0] === mutationData.mutators[i + 1])[2])
+              let target = mutationData.mutators[i];
+              if (!mutatorList || !target) return;
+              let desc: string = (mutatorList as unknown as string[][]).find(mutator => target === mutator[0])![2];
+              // console.log(desc);
+              mutatorField.push({ name: `Mutator ${i + 1}`, value: `${mutationData.mutators[i]}\n\n${desc}`, inline: true });
+            }
+            // console.log('Mutator Field: ', ...mutatorField);
+            // console.log(mutationData);
+        } catch (e) {
+          // console.log(`ERR: ${e}`);
+        }
+      }
 
-    switch (mutationData.rating.at(mutationData.rating.length - 1)) {
-      case '1': color = 0xC9DAF8; break;
-      case '2': color = 0x86B7CA; break;
-      case '3': color = 0xFFE599; break;
-      case '4': color = 0x969FA4; break;
-      case '5': color = 0x9491B4; break;
-      case '6': color = 0xFF0000; break;
-      default: color = 0xFFFFFF; break;
+      let color;
+      if (!mutationData) throw new Error ('No Mutation Data Detected.');
+
+      switch (mutationData.rating.at(mutationData.rating.length - 1)) {
+        case '1': color = 0xC9DAF8; break;
+        case '2': color = 0x86B7CA; break;
+        case '3': color = 0xFFE599; break;
+        case '4': color = 0x969FA4; break;
+        case '5': color = 0x9491B4; break;
+        case '6': color = 0xFF0000; break;
+        default: color = 0xFFFFFF; break;
+      }
+      return new EmbedBuilder()
+      .setColor(color)
+      .setTitle(mutationData.name)
+      .setURL('https://discord.com/channels/1201662672383254658/1201819842475466773/1269418297330503710')
+      .setAuthor({ name: 'Weekly Mutation', /*iconURL: 'https://i.postimg.cc/hv0M7QFr/365422.jpg'*/})
+      //.setThumbnail('https://cdn.discordapp.com/emojis/1262599542914682902.webp?size=80&quality=lossless')
+      .addFields(
+        { name: 'Map', value: mutationData.map },
+        { name: 'Top Pick', value: mutationData.choice },
+        // { name: '\u200B', value: '\u200B' },
+        ...mutatorField as { name: string; value: string; inline?: boolean }[] // cannot statically type as generic objects
+      )
+      .setTimestamp()
+      .setFooter({ text: `(${mutationData.rating}🗡${mutationData.difficulty})` });
     }
-
-    let embed = new EmbedBuilder()
-    .setColor(color)
-    .setTitle(mutationData.name)
-    .setURL('https://discord.com/channels/1201662672383254658/1201819842475466773/1269418297330503710')
-    .setAuthor({ name: 'Weekly Mutation', /*iconURL: 'https://i.postimg.cc/hv0M7QFr/365422.jpg'*/})
-    //.setThumbnail('https://cdn.discordapp.com/emojis/1262599542914682902.webp?size=80&quality=lossless')
-    .addFields(
-      { name: 'Map', value: mutationData.map },
-      { name: 'Top Pick', value: mutationData.choice },
-      // { name: '\u200B', value: '\u200B' },
-      ...mutatorField
-    )
-    .setTimestamp()
-    .setFooter({ text: `(${mutationData.rating}🗡${mutationData.difficulty})` });
 
     // TODO: Perform a manual ms offset for minute insurance or enhance precision logic
     async function adjustTimer(): Promise<number> {
       /** @privateRemarks `toLocaleString` or `Intl.DateTimeFormat()` constructor dismisses 
-      * milliseconds, use only for formatting, not logic precision, you can however pass it
-      * as a dateString when constructing a date, which then may be of use. 
-      *
-      * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#datestring}
-      * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options}
-      * @see {@link https://unicode.org/reports/tr35/#Time_Zone_Identifiers} */
+       * milliseconds, use only for formatting, not logic precision, you can however pass it
+       * as a dateString when constructing a date, which then may be of use. 
+       *
+       * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#datestring}
+       * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat#options}
+       * @see {@link https://unicode.org/reports/tr35/#Time_Zone_Identifiers} */
       let now: Date = new Date();
       let relTime = dailyMs * now.getDay() + 
         (now.getUTCHours() * 60 * 60 * 1000 - 8 * 60 * 60 * 1000) +
@@ -179,6 +188,8 @@ export const command = {
     let reset: number | null = null;
 
     async function intvFn(channel: TextChannel): Promise<void> {
+      let embed = await generateMutatorEmbed();
+      if (!embed) throw new Error('Unable to generate embed.');
       await channel.send({
         embeds: [embed],
       });
@@ -192,12 +203,16 @@ export const command = {
     // TODO: Research mutator `searchMutator()` each interval; may have to reconstruct embed
     caller === 'mutator' ? 
       +(async () => {
+      let embed = await generateMutatorEmbed();
+      if (!embed) throw new Error('Unable to generate embed.');
       await interaction.reply({
         embeds: [embed],
       });
     })() :
       +(async () => {
       try {
+        let embed = await generateMutatorEmbed();
+        if (!embed) throw new Error('Unable to generate embed.');
         if (!interaction.guildId) throw new Error('Guild ID unobtainable.');
 
         let config = await prisma.config.findUnique({
@@ -209,7 +224,7 @@ export const command = {
         await channel.send({ embeds: [embed] });
         adjustTimer();
         if (typeof reset !== 'number') throw new Error('Initial timer failed to adjust.')
-        command.weekIntvId = setTimeout(() => intvFn(channel), reset);
+          command.weekIntvId = setTimeout(() => intvFn(channel), reset);
       } catch (err) {
         console.error(`ERR: Failed to parse config: ${err}`);
       }
