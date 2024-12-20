8ef86f7ee2d65448c9e3404805e47fb67685d1a3
git <82054121+KXzeno@users.noreply.github.com>
2024-12-20 10:03:27 -0800

    feat: fetch msg reference for user mention

--- commands/utility/reminder.ts
+++ commands/utility/reminder.ts
@@ -1,4 +1,4 @@
-import { ChatInputCommandInteraction, GuildBasedChannel, SlashCommandBuilder, TextChannel } from 'discord.js';
+import { ChatInputCommandInteraction, GuildBasedChannel, MessageReference, SlashCommandBuilder, TextChannel } from 'discord.js';
 
 import { command as timetable } from './settimetable.ts'
 import { Timer } from '../../utils/index.ts';
@@ -22,10 +22,28 @@ export const command = {
 
     if (hasTimeTable && interaction.guild && content) {
       targetChannel = interaction.guild.channels.cache.get(timetableChannelId) as GuildBasedChannel as TextChannel;
-      targetChannel.send(`### ${interaction.user.displayName}'s reminder: \`${content}\`\n${Timer.createTimer(Timer.parseInputToISO(timer))}\n-# WARNING: THIS FEATURE IS UNSTABLE, CURRENTLY ONLY SUPPORTS THE REGEX PATTERN: [DIGITS]m`);
+      if (targetChannel === null) {
+        return interaction.reply("Something went wrong with finding the timetable...");
+      }
+
+      let msgRef = await targetChannel.send(`### ${interaction.user.displayName}'s reminder: \`${content}\`\n${Timer.createTimer(Timer.parseInputToISO(timer))}\n-# WARNING: THIS FEATURE IS UNSTABLE, CURRENTLY ONLY SUPPORTS THE REGEX PATTERN: [DIGITS]m`).then(res => res.id);
       interaction.reply(`Reminder sent to <#${timetableChannelId}>`);
+      //
+      // TODO: ERR msg / handling
+      if (msgRef === null) return;
+      setTimeout(() => {
+        // targetChannel already checked
+        targetChannel!.send({ content: `<@${interaction.user.id}>`, reply: { messageReference: msgRef }});
+      }, Timer.parseInputToISO(timer));
     } else {
       interaction.reply(`### ${interaction.user.displayName}'s reminder: \`${content}\`\n${Timer.createTimer(Timer.parseInputToISO(timer))}\n-# WARNING: THIS FEATURE IS UNSTABLE, CURRENTLY ONLY SUPPORTS THE REGEX PATTERN: [DIGITS]m`);
+
+      let msg = await interaction.fetchReply();
+      // TODO: ERR msg / handling
+      if (msg === null) return;
+      setTimeout(() => {
+        msg.reply({ content: `<@${interaction.user.id}>`});
+      }, Timer.parseInputToISO(timer));
     }
   }
 }
