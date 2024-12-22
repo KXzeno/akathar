bb339dfb4245823bc396ef3c736b4f926b6bfb38
git <82054121+KXzeno@users.noreply.github.com>
2024-12-21 19:40:43 -0800

    feat: implement `/clear` for bot outputs

---
+++ commands/utility/clear.ts
@@ -0,0 +1,27 @@
+import { ChatInputCommandInteraction, Collection, GuildMessageManager, Message, SlashCommandBuilder, TextChannel } from "discord.js";
+
+export const command = {
+	data: new SlashCommandBuilder()
+	.setName('clear')
+	.addNumberOption(num => num.setName('count').setDescription('specify a number of messages for the Karnovah to clear'))
+	.setDescription('clears previous bot outputs'),
+	async execute(interaction: ChatInputCommandInteraction) {
+		let channel: TextChannel | null = interaction.channel as TextChannel;
+		if (!channel) {
+			throw new Error('Unable to send through caller\'s channel');
+		}
+
+		let numToClear: number | null = interaction.options.getNumber('num');
+
+		let channelMsgs: GuildMessageManager | Collection<string, Message<true>> = channel.messages;
+		let msgRef = interaction.reply({ content: 'Clearing...', ephemeral: true });
+		await channelMsgs.fetch({ limit: numToClear ??= 10 }).then(messages => {
+			messages.forEach(msg => { 
+				if (msg.author.bot) {
+					msg.delete();
+				}
+			})
+		});
+		msgRef.then(msg => msg.delete());
+	}
+}
