86d3bc7d589d5b8148bebfa733a662c4932654da
git <82054121+KXzeno@users.noreply.github.com>
2024-12-23 05:26:07 -0800

    FEAT: implement dynamic collector

--- commands/utility/cerebrate.ts
+++ commands/utility/cerebrate.ts
@@ -1,27 +1,79 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder, MessageCollector, TextChannel } from "discord.js";
+import { ChatInputCommandInteraction, SlashCommandBuilder, TextChannel, ButtonBuilder, ActionRowBuilder, ButtonStyle, EmbedBuilder, MessageCollector, ComponentType } from "discord.js";
 
 export const command = {
 	data: new SlashCommandBuilder()
 	.setName('cerebrate')
 	.setDescription('connect to an external server'),
-	async execute(interaction: ChatInputCommandInteraction) {
-		if (!interaction.channel) return;
+	async execute(interaction: ChatInputCommandInteraction, channel: TextChannel, reason: string | null) {
+		if (!interaction.guild) throw new Error('Caller\'s guild undetected.');
 
-		let collector: MessageCollector = new MessageCollector(interaction.channel, { filter: msg => msg.author.bot === false });
+		let connect = new ButtonBuilder()
+		.setCustomId('connect')
+		.setLabel('Connect')
+		.setStyle(ButtonStyle.Primary);
 
-		interaction.reply("Collector initialized.");
+		let deny = new ButtonBuilder()
+		.setCustomId('deny')
+		.setLabel('Deny')
+		.setStyle(ButtonStyle.Danger);
 
-		let str: string = '';
+		let leave = new ButtonBuilder()
+		.setCustomId('leave')
+		.setLabel('Leave')
+		.setStyle(ButtonStyle.Danger);
 
-		collector.on('collect', msg => {
-			if (collector.collected.size >= 5) collector.stop();
+		let row = new ActionRowBuilder<ButtonBuilder>()
+		.addComponents(connect, deny);
 
-			for (let [_, msg] of collector.collected.entries()) {
-					str += `${msg.content}\n`
+		let embed = new EmbedBuilder()
+		.setTitle('Connection Request')
+		.setAuthor({
+			name: interaction.guild.name,
+			iconURL: interaction.guild.iconURL() as string
+		})
+		.setDescription(`${interaction.user.username} sent a connection request${reason !== null ? `:\n"${reason}"` : ''}`);
+
+		let res = await channel.send({ 
+			components: [row],
+			embeds: [embed]
+		});
+
+		// TODO: Implement select menu for moving channel
+
+		let outBtnCollector = res.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3600000 });
+
+		outBtnCollector.on('collect', async btn => {
+			let reply = await btn.reply({ content: 'Joined.', ephemeral: true });
+			setTimeout(() => reply.delete(), 5000);
+
+			let postConnectEmbed = await res.edit({
+				components: [row.setComponents(connect, leave)],
+				embeds: [embed.addFields(
+					{
+						name: channel.guild.name, 
+						value: btn.user.username,
+						inline: true
+					},
+					{
+						name: interaction.guild!.name,
+						value: interaction.user.username,
+						inline: true
+					})
+					.setTitle('Connection Established')],
+			});
+
+			if (outBtnCollector.collected.size === 1) {
+				let received = await (interaction.channel as TextChannel).send({
+					components: [row.setComponents(connect, leave)],
+					embeds: [embed.setTitle('Connection Established').setDescription(null)]
+				});
+
+				let inBtnCollector = received.createMessageComponentCollector({ componentType: ComponentType.Button, time: 3600000 });
+				inBtnCollector.on('collect', async btn => {
+					let reply = await btn.reply({ content: 'Joined.', ephemeral: true });
+					setTimeout(() => reply.delete(), 5000);
+				});
 			}
 		});
-		console.log(str);
-		// TODO: FIX COLLECTION OUTPUT
-		collector.on('end', collected => (interaction.channel as TextChannel).send(`Collector terminated: \n${str}`));
 	}
 }
