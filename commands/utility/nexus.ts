bd00279b609b7469ced8e1c4b21a4c2a99f9c217
git <82054121+KXzeno@users.noreply.github.com>
2024-12-24 12:38:40 -0800

    REFACTOR: extract logic to `Nexus` class

--- commands/utility/nexus.ts
+++ commands/utility/nexus.ts
@@ -1,8 +1,9 @@
-import { ChatInputCommandInteraction, Collection, Guild, GuildMessageManager, Message, MessageCollector, PermissionsBitField, SlashCommandBuilder, TextChannel, Webhook } from "discord.js";
-import { scheduler } from "timers/promises";
+import { ChatInputCommandInteraction, Collection, Guild, Message, MessageCollector, PermissionsBitField, SlashCommandBuilder, TextChannel, Webhook } from "discord.js";
 
+import { Nexus } from "../../utils/Nexus.ts";
 import { event as guildFetch } from '../../events/guildFetch.ts';
 import { command as transmitReq } from './cerebrate.ts';
+import { WebhookManager } from "../../utils/index.ts";
 
 type GuildData = {
 	name: string;
@@ -20,138 +21,68 @@ export const command = {
 	.addStringOption(guild => guild.setName('guild').setDescription('server to transmit').setRequired(true))
 	.addStringOption(reason => reason.setName('reason').setDescription('the reason for contact')),
 	async execute(interaction: ChatInputCommandInteraction) {
-		let { guildData } = guildFetch;
-		let guildInput: string | null = interaction.options.getString('guild');
 
+		let guildInput: string | null = interaction.options.getString('guild');
 		if (guildInput === null) throw new Error('Unparseable input.');
 
-		let botId = interaction.applicationId;
-		let targetGuild: Guild = guildData.filter(guild => guild.id === guildInput || guild.name.toLowerCase() === guildInput.toLowerCase())[0]?.data || null;
+		// Create connection request
+		let reason = interaction.options.getString('reason') || null;
 
-		if (targetGuild === null) return interaction.reply({ content: `Unable to find guild: ${guildInput}`, ephemeral: true });
-
-		let targetChannel: TextChannel | null = null;
-		targetChannel = targetGuild.systemChannel;
-
+		let nexus = new Nexus(interaction, guildInput, reason);
+		let targetChannel = nexus.getChannelTarget();
 		if (targetChannel) {
-			let permissions: Readonly<PermissionsBitField> | null = targetChannel.permissionsFor(botId);
-			if (permissions === null) return;
-			let sendable: boolean = permissions.has([PermissionsBitField.Flags.SendMessages]);
+			let sendable = nexus.isChannelSendable(targetChannel);
 			if (!sendable) {
-				for (let [_, channel] of targetGuild.channels.cache.entries()) {
-					if (channel instanceof TextChannel && channel.name) {
-						let permissions: Readonly<PermissionsBitField> | null = channel.permissionsFor(botId);
-						if (permissions === null) return;
-						let sendable: boolean = permissions.has([PermissionsBitField.Flags.SendMessages]);
-						if (sendable) {
-							targetChannel = channel;
-							break;
-						}
-					}
-				}
+				nexus.assertiveSearchDefaultChannel();
+				targetChannel = nexus.getChannelTarget();
 			}
+
 			if (!targetChannel) throw new Error('Unable to find system / sendable channel');
 			if (!interaction.channel) throw new Error('Unable to find caller\'s channel');
 
-			// Initialize multi-collector logic
-			let [outCollector, inCollector]: [MessageCollector | null, MessageCollector | null] = [new MessageCollector(interaction.channel), new MessageCollector(targetChannel)];
-
-			// Create connection request
-			let reason = interaction.options.getString('reason') || null;
-
-			// TODO: Store members to allow multiple
-			// TODO: Add member containment
-			let outWebhook: Webhook | null = null;
-			let inWebhook: Webhook | null = null;
+			let webhooks = new WebhookManager();
 
 			// Type?
 			let outCollectorFn: (args_0: Message<boolean>, args_1: Collection<string, Message<boolean>>) => void = async (msg: Message) => {
 				if (msg.author.bot) return;
-				if (!outWebhook) {
-					outWebhook = await targetChannel!.createWebhook({ 
-						name: msg.author.username,
-						avatar: msg.author.displayAvatarURL(),
-					});
+				if (await webhooks.has(msg.author.username, targetChannel as TextChannel) === false) {
+					await webhooks.add(msg, targetChannel as TextChannel);
 				}
-				await outWebhook.send({
-					content: msg.content,
-					files: [...msg.attachments.values() || null],
-				});
+				let webhook = webhooks.get(msg.author.username, targetChannel as TextChannel);
+				// console.log(`From ${msg.channel} to ${targetChannel}, outbound`);
+				await WebhookManager.fire(webhook, msg);
+
 				if (msg.content === '$cancel') {
-					(interaction.channel as TextChannel).send('Connection terminated.');
-					targetChannel!.send('Connection terminated.');
-					outWebhook.delete();
-					if (inWebhook) {
-						inWebhook.delete();
-					}
-					outCollector.stop();
+					Nexus.terminate(nexus);
+					webhooks.eradicate();
 				}
 			};
 
-			let resetOutCollector = (collector: MessageCollector, channel: TextChannel): MessageCollector => {
-				collector.stop();
-				let newCollector = new MessageCollector(channel);
-				newCollector.on('collect', outCollectorFn);
-				return newCollector;
+			transmitReq.execute({ interaction, nexus, targetChannel, reason });
+			if (nexus.outboundCollector === null) {
+				throw new Error('Outbound collector not initialized');
 			}
 
-			let reset = (collector: Parameters<typeof resetOutCollector>[0], channel: Parameters<typeof resetOutCollector>[1]): ReturnType<typeof resetOutCollector> => {
-				targetChannel = channel;
-				console.log(`Received Arguments: ${channel.name}\nUpdated value: ${targetChannel?.name}`);
-				return resetOutCollector(collector, collector.channel as TextChannel);
+			if (nexus.inboundCollector === null) {
+				throw new Error('Inbound collector not initialized');
 			}
 
-			transmitReq.execute({ interaction, targetChannel, reason, outCollector, inCollector, outWebhook, inWebhook }, reset);
+			nexus.outboundCollector.on('collect', outCollectorFn);
 
-			outCollector.on('collect', outCollectorFn);
-
-			inCollector.on('collect', async (msg: Message) => {
+			nexus.inboundCollector.on('collect', async (msg: Message) => {
 				if (msg.author.bot) return;
-				if (!inWebhook) {
-					inWebhook = await (interaction.channel as TextChannel)!.createWebhook({ 
-						name: msg.author.username,
-						avatar: msg.author.displayAvatarURL(),
-					});
+				if (await webhooks.has(msg.author.username, interaction.channel as TextChannel) === false) {
+					await webhooks.add(msg, interaction.channel as TextChannel);
 				}
-				await inWebhook.send({
-					content: msg.content,
-					files: [...msg.attachments.values() || null],
-				});
+				// console.log(`From ${msg.channel} to ${interaction.channel}, inbound`);
+				await WebhookManager.fire(webhooks.get(msg.author.username, interaction.channel as TextChannel), msg);
 				if (msg.content === '$cancel') {
-					targetChannel!.send('Connection terminated.');
-					(interaction.channel as TextChannel).send('Connection terminated.');
-					inWebhook.delete();
-					if (outWebhook) {
-						outWebhook.delete();
-					}
-					outCollector.stop();
+					Nexus.terminate(nexus);
+					webhooks.eradicate();
 				}
 			});
 
 			interaction.reply(`Connected to server: ${guildInput}`);
-
-			// let webhook = await targetChannel.createWebhook({ 
-			// 	name: '\u{01CBC}',
-			// 	avatar: 'https://www.sonoranspice.com/cdn/shop/files/UnderwoodRanchesSrirachaFront_comp_2000x.jpg?v=1692125376',
-			// });
-			// let msg = await webhook.send(`Test`);
-			// let reply = await interaction.reply({ content: 'Sent.', ephemeral: true });
-			// webhook.delete();
-			// await scheduler.wait(5000);
-			// reply.delete();
 		}
-
-
-		/** Fetch Msgs
-		 * let channelMsgs: GuildMessageManager | Collection<string, Message<true>> = targetChannel.messages;
-		 * let str: string[] = [];
-		 * await channelMsgs.fetch({ limit: 5 }).then(messages => {
-		 * 	messages.forEach(msg => { 
-		 * 		str.push(msg.content);
-		 * 	})
-		 * });
-		 * str = str.filter(line => line.length > 0);
-		 * interaction.reply(str.reduce((p, n) => `${p}\n${n}`));
-		 */
 	}
 }
