d48df4e5a770c52a989c6394d6165621d5e83ace
git <82054121+KXzeno@users.noreply.github.com>
2024-12-25 05:05:05 -0800

    test: log deployed cmds and add static typing

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,4 +1,4 @@
-import { REST, Routes } from 'discord.js';
+import { ApplicationCommand, REST, Routes } from 'discord.js';
 import 'dotenv/config';
 import fs from 'node:fs';
 import path, { dirname } from 'node:path';
@@ -8,7 +8,7 @@ import { fileURLToPath } from 'node:url';
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
-const commands: string[] = [];
+const commands: ApplicationCommand[] = [];
 // Grab all the command folders from the commands directory you created earlier
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
@@ -48,7 +48,11 @@ await Promise.all(importPromises);
 if (process.env.TOKEN === undefined) throw new Error('Missing or Invalid Token');
 const rest = new REST().setToken(process.env.TOKEN);
 
-// and deploy your commands!
+// Log deployed commands
+for (let command of commands) {
+	console.log(command.name);
+};
+
 (async () => {
 	try {
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
