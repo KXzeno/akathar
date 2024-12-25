07f3ade84f4e15ffcf697688950857e026d97735
git <82054121+KXzeno@users.noreply.github.com>
2024-12-25 09:19:44 -0800

    Add type `GuildData`

--- utils/types.ts
+++ utils/types.ts
@@ -1,6 +1,14 @@
 import { Guild } from "discord.js";
 
+export type GuildData = {
+  data: Guild;
+  name: string;
+  id: string;
+  defaultChannel: string | undefined;
+}
+
 export type Sojourn = {
   name: string;
   guild: Guild;
 }
+
