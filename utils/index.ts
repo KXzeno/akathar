6c1a9d05569227d7f27ccdad40bc4f5288755b13
git <82054121+KXzeno@users.noreply.github.com>
2024-12-25 08:22:37 -0800

    chore: prune stale type from exports

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,9 @@
 import { Timer } from './Timer.ts';
-import { Nexus, Sojourn } from './Nexus.ts';
+import { Nexus } from './Nexus.ts';
 import { WebhookManager } from './WebhookManager.ts'
 
 export {
   Timer,
-  Nexus, Sojourn,
+  Nexus, 
   WebhookManager,
 }
