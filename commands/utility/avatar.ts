8d83ae30fba6aac5385ea73a572ca0a015134f86
git <82054121+KXzeno@users.noreply.github.com>
2025-01-13 13:56:06 -0800

    feat: add guild avatar option

--- commands/utility/avatar.ts
+++ commands/utility/avatar.ts
@@ -1,13 +1,23 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
+import { ChatInputCommandInteraction, SlashCommandBuilder, TextChannel } from 'discord.js';
 
 export const command = {
 	data: new SlashCommandBuilder()
 	.setName('avatar')
 	.setDescription('sends avatar')
-	.addUserOption(input => input.setName('user').setDescription('get avatar for a specific user')),
+	.addUserOption(input => input.setName('user').setDescription('get avatar for a specific user').setRequired(true))
+	.addBooleanOption(isCustom => isCustom.setName('guild-avatar').setDescription('display the user\'s guild avatar instead')),
 	async execute(interaction: ChatInputCommandInteraction) {
 		let userOpt = interaction.options.getUser('user');
+		let isGuild = interaction.options.getBoolean('guild-avatar');
+		if (!interaction.guild) {
+			throw new Error('No guild found.');
+		}
+
 		if (userOpt) {
+			let user = interaction.guild.members.cache.find(user => user.id === userOpt.id);
+			if (user && isGuild) {
+				return (interaction.channel as TextChannel).send(user.displayAvatarURL({ extension: 'png', size: 1_024 }));
+			}
 			let avatarUrl = userOpt.displayAvatarURL({ extension: 'png', size: 1_024 });
 			interaction.reply(avatarUrl);
 		} else {
